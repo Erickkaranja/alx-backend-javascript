975125f8bf21f47c85927ddbdb818f0df219a86a
Erickkaranja <karanja99erick@gmail.com>
2023-09-30 20:00:38 +0300

    correcting task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft === "number") {
+    if (typeof sqft === 'number') {
       this._sqft = sqft;
     }
   }
+
   get sqft() {
     return this._sqft;
   }
+
   evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
