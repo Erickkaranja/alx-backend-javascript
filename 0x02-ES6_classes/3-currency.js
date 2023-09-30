a471da1200fb9b857cb58d24d4d16a878cea1a81
Erickkaranja <karanja99erick@gmail.com>
2023-09-30 22:19:23 +0300

    updating task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,23 +1,27 @@
 export default class Currency {
   constructor(code, name) {
-    if (typeof code === "string" && typeof name === "string") {
+    if (typeof code === 'string' && typeof name === 'string') {
       [this._code, this._name] = [code, name];
     }
   }
 
-  get Code() {
+  get Code () {
     return this._code;
   }
+
   get name() {
     return this._name;
   }
+
   set code(code) {
     this._code = code;
   }
+
   set name(name) {
     this._name = name;
   }
-  displayFullCurrency () {
-    return `${this._name} (${this._code})`
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }
