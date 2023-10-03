c7c6d56a68bde49c2c53408e642c911ba3845eb7
Erickkaranja <karanja99erick@gmail.com>
2023-10-03 22:21:54 +0300

    updating task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(studentList, city) {
+  const studentsInCity = studentList.filter(
+      (student) => student.location === city,
+  );
+
+  return studentsInCity;
+}
