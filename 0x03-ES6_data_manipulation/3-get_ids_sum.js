2686b8ec4550c6fcfc7592186a093ddc850b51f3
Erickkaranja <karanja99erick@gmail.com>
2023-12-29 21:54:55 +0300

    updating task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,7 @@
+/**
+ * This function gets a list of students.
+ * @return {list} returns a list of given students.
+ */
 export default function getStudentIdsSum(studentList) {
   const sumId = studentList.reduce((accumulator, student) => {
     return accumulator + student.id;
