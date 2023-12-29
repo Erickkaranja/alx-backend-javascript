c6f9ad5643ce8abab6bed64b4bf0ed7557fbb2f1
Erickkaranja <karanja99erick@gmail.com>
2023-12-29 06:56:29 +0300

    updating task 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,18 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default async function asyncUploadUser () {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  const response = {
-    photo,
-    user
-  };
-
-  return response;
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    const response = {
+      photo,
+      user
+    };
+    return response;
+  } catch (error) {
+      return {
+        photo: null,
+        user: null
+      };
+    }
 }
