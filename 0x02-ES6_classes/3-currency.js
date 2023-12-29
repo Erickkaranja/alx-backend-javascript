515a6de6a9c7617eac72280dd372a24070663d7a
Erickkaranja <karanja99erick@gmail.com>
2023-12-29 20:07:55 +0300

    updating tasks

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -14,16 +14,16 @@ export default class Currency {
 
   set code(code) {
     if (typeof code !== 'string') {
-      throw new Error("code must be a string");
-    }   
- 
+      throw new Error('code must be a string');
+    }
+
     this._code = code;
   }
 
   set name(name) {
     if (typeof name !== 'string') {
-      throw new Error("name must be a string");
-    }   
+      throw new Error('name must be a string');
+    }
     this._name = name;
   }
 
