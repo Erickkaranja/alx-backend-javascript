37b79b38b440533b7dfdf9427899968e4b7e8afa
Erickkaranja <karanja99erick@gmail.com>
2023-12-29 22:07:40 +0300

    updating task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,8 @@
 /**
  * This function gets a list of students from a given city.
- * @return {list} returns a list of given students.
+ * @param {Array} studentList - An array of students.
+ * @param {string} city - The city to filter students by.
+ * @return {Array} Returns a list of students from the given city.
  */
 export default function getStudentsByLocation(studentList, city) {
   const studentsInCity = studentList.filter(
