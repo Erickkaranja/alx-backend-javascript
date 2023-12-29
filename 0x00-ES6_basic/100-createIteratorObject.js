b4ccac1a924dd25da30be290066941597d46c740
Erickkaranja <karanja99erick@gmail.com>
2023-12-29 20:35:57 +0300

    updating docs

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,4 @@
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   const iterable = Object.keys(report.allEmployees);
   const array = [];
   for (const key of iterable) {
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,4 @@
-export default function iterateThroughObject (reportWithIterator) {
+export default function iterateThroughObject(reportWithIterator) {
   const result = reportWithIterator.join(' | ');
 
   return result;
