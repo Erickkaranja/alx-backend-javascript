7a6af28275ee36da41c101ef0c3bf150ec4419a2
Erickkaranja <karanja99erick@gmail.com>
2024-01-02 12:35:22 +0300

    updating task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,25 +1,26 @@
 const http = require('http');
+
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer(async(req, res) => {
   const { url } = req;
   let message = 'This is the list of our students\n';
   switch (url) {
-    case '/':
-      res.statusCode = 200;
-      res.end('Hello Holberton School!');
+  case '/':
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+    break;
+  case '/students':
+    try {
+      message += await countStudents(process.argv[2]);
+      res.end(message);
       break;
-    case '/students':
-      try {
-        message += await countStudents(process.argv[2]);
-        res.end(message);
-        break;
-      } catch {
-        res.end(err.message);
-        break;
-      }
-    default:
+    } catch {
+      res.end(err.message);
       break;
+    }
+  default:
+    break;
   }
 });
 
